0d8c20f51f5b718e6a5231f6af92cf425c61ebed
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2024-02-09 00:37:07 +0800

    feat: ai event tagging

--- src/server/api/routers/event.ts
+++ src/server/api/routers/event.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { EventCreateInputSchema } from "prisma/generated/zod";
+import OpenAI from "openai";
 
 const idSchema = z.object({ id: z.number() });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const eventRouter = createTRPCRouter({
   getOne: publicProcedure.input(idSchema).query(({ input, ctx }) => {
@@ -55,8 +59,41 @@ export const eventRouter = createTRPCRouter({
     .input(EventCreateInputSchema)
     .mutation(async ({ input, ctx }) => {
       await ctx.db.$connect();
-      return ctx.db.event.create({
+      const createdEvent = await ctx.db.event.create({
         data: EventCreateInputSchema.parse(input),
+      });
+      const { id: eventId } = createdEvent;
+      const { name: eventName, details: eventDetails } = input;
+      const hashtags = await openai.chat.completions
+        .create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: `Generate 3 unique tags related to the following event:\nTitle: ${eventName}\nDetails: ${eventDetails}.\nReturn the data without # and in a JSON array with \"hashtags\" as the key.`,
+            },
+          ],
+          temperature: 1,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        })
+        .then(({ choices }) => choices)
+        .then((choices) => {
+          if (!choices || choices.length === 0) throw new Error();
+          const { content } = choices[0]?.message ?? {
+            content: "",
+          };
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          return JSON.parse(content!).hashtags as Array<string>;
+        })
+        .catch((err) => {
+          console.error(err);
+          return ["#volunteering", "#environment", "#together"];
+        });
+      await ctx.db.eventTag.createMany({
+        data: hashtags.map((name) => ({ name, eventId })),
       });
     }),
   delete: protectedProcedure.input(idSchema).mutation(({ input, ctx }) => {
