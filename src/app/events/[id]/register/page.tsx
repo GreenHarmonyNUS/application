ef5d031c412fbabb7bb7a5c3aadef56891e4347b
nichyjt <nicholas.yek@gmail.com>
2024-02-09 12:28:07 +0800

    Add defensive code, prevent bad /register access

--- src/app/events/[id]/register/page.tsx
+++ src/app/events/[id]/register/page.tsx
@@ -1,11 +1,20 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { api } from "~/trpc/server";
+import { authOptions } from "~/server/auth";
 
 const EventRegister = async ({ params }: { params: { id: string } }) => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   const { id: eventId } = params;
-  if (!session) redirect("/events");
+
+  // Guard clauses
+  if (!session) redirect(`/events/${eventId}`);
+  const isRegistered = await api.event.isRegistered.query({
+    eventId: Number(eventId),
+    userId: session?.user.id,
+  });
+  if (isRegistered) redirect(`/events/${eventId}`);
+
   // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
   await api.eventRegistrations.create.mutate({
     eventId: +eventId,
