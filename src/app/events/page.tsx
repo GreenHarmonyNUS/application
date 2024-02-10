7b72fb790f58af472d76158ad06a18914dc6f4a8
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2024-02-10 21:45:55 +0800

    feat: add create event button

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -5,6 +5,8 @@ import { api } from "~/trpc/server";
 import type { EventResponse } from "../_types/event-response";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/server/auth";
+import { Button } from "@mui/material";
+import Link from "next/link";
 
 const MyEventsPage = async () => {
   const session = await getServerSession(authOptions);
@@ -16,16 +18,21 @@ const MyEventsPage = async () => {
   const eventData: EventResponse[] = await api.event.getAll.query();
 
   return (
-    <div>
+    <div className="mb-16 mt-4 flex flex-col justify-center">
       {session ? (
-        <>
+        <div>
           <h1 className="pt-4 text-center text-2xl">Your suggested events</h1>
           <EventCarousel events={suggestedEvents} />
-        </>
+        </div>
       ) : (
         <h1 className="py-4 text-center text-2xl">Our events</h1>
       )}
       <EventsTab events={eventData} />
+      {session && (
+        <Button>
+          <Link href="/events/create">Create an event</Link>
+        </Button>
+      )}
     </div>
   );
 };
